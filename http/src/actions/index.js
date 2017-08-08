8dc11a871544fb8af4cd5fb8d89ebd1b68c58e08
kiyoshi626 <travis.nakano@gmail.com>
2017-08-07 19:58:11 -0600

    updated

--- http/src/actions/index.js
+++ http/src/actions/index.js
@@ -3,5 +3,9 @@ import axios from 'axios';
 export const GET_FRIENDS = 'GET_FRIENDS';
 
 export const getFriends = () => {
-    // Add the code for this action
+    const promise = axios.get('http://localhost:5000/friends');
+    return {
+        type: GET_FRIENDS,
+        payload: promise
+    };
 };
